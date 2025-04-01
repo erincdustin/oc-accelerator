cd1ada8c6eaad87e23be046cb163b53a467c24ea
Erin <erincdustin@gmail.com>
2025-04-01 15:04:17 -0700

    [WIP] admin restructuring

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Chakra } from './components/Shared/branding/Chakra.tsx'
-import AppProvider from './providers/AppProvider.tsx'
+import { Chakra } from './components/shared/branding/Chakra.tsx'
+import AppProvider from './AppProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- apps/admin/src/routes/resources.tsx
+++ apps/admin/src/routes/resources.tsx
@@ -1,8 +1,8 @@
 import { IndexRouteObject, NonIndexRouteObject } from 'react-router-dom'
-import ResourceList from '../components/ResourceList/ResourceList'
-import { ResourceDetailWithParams } from '../components/ResourceDetail/ResourceDetail'
-import AssignmentList from '../components/AssignmentList/AssignmentList'
-import ResourceAssignment from '../components/ResourceAssignment/ResourceAssignment'
+import ResourceList from '../components/resource-list/ResourceList'
+import { ResourceDetailWithParams } from '../components/resource-detail/ResourceDetail'
+import AssignmentList from '../components/assignment-list/AssignmentList'
+import ResourceAssignment from '../components/resource-assignment/ResourceAssignment'
 
 interface IndexResourceRoute extends IndexRouteObject {
   label?: string
